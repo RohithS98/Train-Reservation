14a46acd462f0a752a49f5a559fad8f66a8d671e
shubhamrsangle <36818228+shubhamrsangle@users.noreply.github.com>
2018-11-25 19:25:30 +0530

    Add files via upload

--- deletebooking.php
+++ deletebooking.php
@@ -3,14 +3,22 @@ session_start();
 	$connect = mysqli_connect("localhost", "root", "25285618", "traindb");
 	$id = $_POST["ticketno"];
 	$username = $_SESSION["user_info"];
+	$sql = "SELECT status from history WHERE ticketno='".$id."';";
+	$temp = mysqli_query($connect,$sql) or die ('request "Could not execute SQL query" '.$sql);
+	$initialStat = mysqli_fetch_array($temp)["status"];
 	$sql = "UPDATE history SET status='Cancelled' WHERE ticketno='".$id."';";
 	if(mysqli_query($connect, $sql))
 	{
-		$sql = "SELECT cost from history WHERE ticketno='".$id."';";
-		$sql_result = mysqli_query($connect, $sql);
-		$amount = mysqli_fetch_array($sql_result)["cost"];
-		$sql = "UPDATE userdetails SET amount=amount + $amount WHERE user='".$username."';";
-		mysqli_query($connect, $sql);
-		echo 'Data Deleted';
+		if($initialStat != 'Cancelled'){
+			$sql = "SELECT cost from history WHERE ticketno='".$id."';";
+			$sql_result = mysqli_query($connect, $sql);
+			$amount = mysqli_fetch_array($sql_result)["cost"];
+			$sql = "UPDATE userdetails SET amount=amount + $amount WHERE user='".$username."';";
+			mysqli_query($connect, $sql);
+			echo 'Data Deleted';
+		}
+		else{
+			echo "Cannot cancel an already cancelled ticket";
+		}
 	}
  ?>
