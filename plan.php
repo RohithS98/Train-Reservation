ff25c81a6c9bda664b8cbba46b47e971540f63cc
shubhamrsangle <36818228+shubhamrsangle@users.noreply.github.com>
2018-11-25 12:56:35 +0530

    Add files via upload

--- plan.php
+++ plan.php
@@ -27,10 +27,19 @@
                 <div class="col-12"><input class="form-control" type="text" id="to" placeholder="To Station"></div>
             </div>
                 <hr>
-                <div class="input-group input-group-lg" style="text-align: center">
-                    <div class="col-8"><input class="form-control" type="text" id="name"placeholder="Passanger Name"></div>
-                    <div class="col-3"><input class="form-control" type="text" id="age "placeholder="Age"></div>
-                </div>
+                <div class="form-group">
+				<form name="add_name" id="add_name">
+					<div class="table-responsive">
+						<table class="table table-bordered" id="dynamic_field">
+							<tr>
+								<td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list" /></td>
+								<td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>
+							</tr>
+						</table>
+						<input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />
+					</div>
+				</form>
+		</div>
             <hr>
             <br>   
             <button type="button" class="btn btn-success" style="width: 100%;">Pay</button>
@@ -47,3 +56,31 @@
         <div class="col"></div>
 </body>
 </html>
+<script>
+$(document).ready(function(){
+	var i=1;
+	$('#add').click(function(){
+		i++;
+		$('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name[]" placeholder="Enter your Name" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+	});
+	
+	$(document).on('click', '.btn_remove', function(){
+		var button_id = $(this).attr("id"); 
+		$('#row'+button_id+'').remove();
+	});
+	
+	$('#submit').click(function(){		
+		$.ajax({
+			url:"name.php",
+			method:"POST",
+			data:$('#add_name').serialize(),
+			success:function(data)
+			{
+				alert(data);
+				$('#add_name')[0].reset();
+			}
+		});
+	});
+	
+});
+</script>
