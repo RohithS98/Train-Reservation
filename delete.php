9470e3709e9e13fc800b952c3c78985a8d3865ea
Rohith Sudheer <36558130+RohithS98@users.noreply.github.com>
2018-11-25 18:58:18 +0530

    Delete delete.php

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$connect = mysqli_connect("localhost", "root", "cs16b026", "traindb");
-	$sql = "DELETE FROM train WHERE trainno = '".$_POST["Trainnum"]."'";
-	if(mysqli_query($connect, $sql))
-	{
-		echo 'Data Deleted';
-	}
- ?>
